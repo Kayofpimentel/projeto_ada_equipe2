21d989bab6a4ea00345a08e9f7309a3b7d995eda
Lia Guimaraes <lia.guimaraes@gmail.com>
2026-05-11 22:23:59 -0300

    alteração da Lia

---
+++ script.js
@@ -0,0 +1,57 @@
+const form = document.querySelector('.cadastro');
+const nome = document.getElementById('nome');
+const email = document.getElementById('email');
+const plano = document.getElementById('plano');
+const listaUl = document.querySelector('listaClientes');
+
+function validarEmail(email) {
+    return email.includes('@');
+}
+
+email.addEventListener('blur', function() {
+    const emailValue = email.value;
+    if (emailValue !== "" && !validarEmail(emailValue)) {
+        email.classList.add('invalid');
+    } else {
+        email.classList.remove('invalid');
+    }
+});
+
+
+function renderizarUsuarios() {
+    listaUl.innerHTML = ''; 
+    const usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+    
+    usuarios.forEach(usuario => {
+        const li = document.createElement('li');
+        li.innerHTML = `<strong>${usuario.nome}</strong> - ${usuario.email} (Plano: ${usuario.plano})`;
+        listaUl.appendChild(li);
+    });
+}
+
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const dadosUsuario = { 
+        nome: nome.value, 
+        email: email.value, 
+        plano: plano.value 
+    };
+
+
+    const listaUsuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+    listaUsuarios.push(dadosUsuario);
+    localStorage.setItem('usuarios', JSON.stringify(listaUsuarios));
+
+    console.log('Dados do usuário:', dadosUsuario);
+    alert('Cadastrado com sucesso!');
+    
+    form.reset(); // Limpa os campos
+    renderizarUsuarios(); // Atualiza a lista na tela
+}); 
+
+// 4. Chamada inicial para carregar dados ao abrir a página
+renderizarUsuarios();
+
+// teste da Lia
