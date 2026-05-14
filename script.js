2578b98f96ea0a628d26b10064f628ff9bdb0fa8
Lia Guimaraes <lia.guimaraes@gmail.com>
2026-05-13 22:05:09 -0300

    incluindo modal e botaõ excluir ainda a aprimorar

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+// Modal de boas-vindas
+const modal = document.getElementById('modalBoasVindas');
+const btnIniciar = document.getElementById('btnIniciar');
+const nomeUsuarioInput = document.getElementById('nomeUsuario');
+const saudacaoTopo = document.getElementById('saudacaoTopo');
+
+// Verifica se já existe nome salvo na sessão
+let nomePessoa = sessionStorage.getItem('nomePessoa');
+
+if (!nomePessoa) {
+    modal.style.display = 'flex'; // abre o modal
+} else {
+    saudacaoTopo.textContent = `Olá, ${nomePessoa}`;
+}
+
+// Quando clicar em iniciar
+btnIniciar.addEventListener('click', () => {
+    const nomeDigitado = nomeUsuarioInput.value.trim();
+
+    if (nomeDigitado === "") {
+        alert("Digite seu nome antes de continuar");
+        return;
+    }
+
+    sessionStorage.setItem('nomePessoa', nomeDigitado);
+    saudacaoTopo.textContent = `Olá, ${nomeDigitado}`;
+    modal.style.display = 'none';
+});
+
+
+
+
 const form = document.getElementById('formCadastro');
 const nome = document.getElementById('nome');
 const email = document.getElementById('email');
@@ -30,10 +62,26 @@ function renderizarUsuarios() {
         if (usuario.plano === 'silver') classePlano = 'plano-silver';
         if (usuario.plano === 'bronze') classePlano = 'plano-bronze';
         
-        li.innerHTML = `<strong>${usuario.nome}</strong> - ${usuario.email} - Plano  ${usuario.plano}`;
+        li.innerHTML = `<strong>${usuario.nome}</strong> - ${usuario.email} - Plano  ${usuario.plano} <button class="btn-excluir">Excluir</button>`;
+        const botaoExcluir = li.querySelector('.btn-excluir');
+
+        botaoExcluir.addEventListener('click', function () {
+            excluirUsuario(usuario.id);
+});
+
         li.classList.add(classePlano);
         listaUl.appendChild(li);
     });
+}
+
+function excluirUsuario(idUsuario) {
+    let usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+
+    usuarios = usuarios.filter(u => u.id !== idUsuario);
+
+    localStorage.setItem('usuarios', JSON.stringify(usuarios));
+
+    renderizarUsuarios();
 }
 
 
@@ -42,6 +90,7 @@ form.addEventListener('submit', function(event) {
     event.preventDefault();
 
     const dadosUsuario = { 
+        id: Date.now(),
         nome: nome.value, 
         email: email.value, 
         plano: plano.value 
